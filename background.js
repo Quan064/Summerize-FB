b2034d04ec81f312795ab743c859d8874f788164
Quan064 <minhquanthong064@gmail.com>
2025-03-01 03:15:13 +0700

    final

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 let createdTabs = new Set();
 
 // Hàm lấy tin nhắn chưa đọc bằng cách chạy content script trong tab Messenger
-async function checkUnreadMessages(url) {
+async function checkUnreadMessages(url, func) {
     let tabs = await chrome.tabs.query({ url: url });
     if (tabs.length === 0) {
         // Nếu chưa có tab Messenger mở, mở tab ẩn
@@ -10,38 +10,19 @@ async function checkUnreadMessages(url) {
             chrome.tabs.create({ url: url, active: false }, (newTab) => {
                 createdTabs.add(newTab.id); // Lưu ID của tab vừa tạo
                 setTimeout(async () => { 
-                    let messages = await extractMessages(newTab.id);
+                    let messages = await func(newTab.id);
                     // Kiểm tra nếu tab vẫn tồn tại và chưa active thì đóng nó
                     chrome.tabs.get(newTab.id, (tab) => {
                         if (tab && !tab.active) {
                             chrome.tabs.remove(newTab.id);
                         }
                     });
-                    // chrome.scripting.executeScript({
-                    //     target: { tabId: newTab.id },
-                    //     func: () => {
-                    //         let target = document.querySelector("#\:r1f\: > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1)");
-                    //         let button = document.querySelector("#\:r1f\: > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div > div.html-div.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x6s0dn4.x1hgdm3.x78zum5.x1q0g3np.x84fkku.x67bb7w.x10l6tqk.x1jl3hli.x1qvwoe0.x1vzd84k.xjm9jq1.x6ikm8r.x10wlt62.x1i1rx1s");
-
-                    //         if (target && button) {
-                    //             let rect = target.getBoundingClientRect();
-                    //             let event = new MouseEvent("mousemove", {
-                    //                 bubbles: true,
-                    //                 clientX: rect.left + 5, // Giả lập vị trí trong phần tử
-                    //                 clientY: rect.top + 5
-                    //             });
-                    //             target.dispatchEvent(event); // Giả lập hover
-
-                    //             setTimeout(() => { button.click(); }, 500); // Đợi một chút rồi click
-                    //         }
-                    //     }
-                    // });
                     resolve(messages);
                 }, 6000);
             });
         });
     } else {
-        return extractMessages(tabs[0].id);
+        return func(tabs[0].id);
     }
 }
 
@@ -67,10 +48,52 @@ async function extractMessages(tabId) {
 // Lắng nghe tin nhắn từ popup
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "popupOpened") {
-        let promises = request.urls.map(url => checkUnreadMessages(url));
+        let promises = request.urls.map(url => checkUnreadMessages(url, extractMessages));
         Promise.all(promises).then(results => {
             sendResponse(results); // Gửi phản hồi sau khi tất cả hoàn tất
         });
         return true; // Giữ kết nối để gửi phản hồi bất đồng bộ
+    };
+    if (request.type === "getInfo") {
+        checkUnreadMessages(request.url, getInfo).then(Info => {
+            sendResponse(Info);
+        });
+        return true;
+    }
+    if (request.type === "checkUnread") {
+        checkUnreadMessages("https://www.facebook.com/messages", checkUnreadLinks).then(urls => {
+            sendResponse(urls);
+        });
+        return true;
     }
 });
+
+async function getInfo(tabId) {
+    let results = await chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+            let info = {};
+            info.avatar = document.evaluate('//*/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div/div/div[1]/div/div/div/*/div[1]/div/div[1]//img', document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext().src;
+            info.name = document.evaluate('//*/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div/div/div[1]/div/div/div//div[1]/div/div[2]/div/div[1]/h2', document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null).iterateNext().innerText;
+            return info;
+        }
+    });
+    return results[0].result;
+}
+
+async function checkUnreadLinks(tabId) {
+    let results = await chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+            let urls = [];
+            const xpath = '//*/div/div/div/div/div/div[2]/div/div/div/div/div/div/div[1]/div/div/div/a/div[1]/div/div[3]/div/div//span/../../../../../../..';
+            let result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+            let a;
+            while ((a = result.iterateNext())) {
+                urls.push(a.href);
+            };
+            return urls;
+        }
+    });
+    return results[0].result;
+}
--- popup.js
+++ popup.js
@@ -1,8 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let messages = [
-        { avatar: "https://scontent.fsgn8-4.fna.fbcdn.net/v/t1.15752-9/462537963_991331293036646_6818074281671940407_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=b70caf&_nc_eui2=AeGOdlZqTk6-K_7kZUTgMEkmKN2OH0w6LY4o3Y4fTDotjg1fY17UXxOYfs-8p1NLp_fNfh9Ada839t8mebQL7EVT&_nc_ohc=ecZD9Wei2Z4Q7kNvgEXfiUk&_nc_oc=AdgWa1GVJnavQCqkCxqiXgZmw2GwOG61Ol0ppb9JxlfQ1e8PjZPUBDwLUIsSruTXQyaexclJITLdKC8aGR34BDYk&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&oh=03_Q7cD1gHj9pz-W99XhyqsTrOLfot-eYL6wFWPifjKpgsjd5VBmA&oe=67E24525", name: "Em iu❤️🍓", url: "https://www.facebook.com/messages/t/8399740233487936" },
-        { avatar: "https://scontent.fsgn8-4.fna.fbcdn.net/v/t1.15752-9/462650106_556580263874693_7872485036846904050_n.jpg?stp=dst-jpg_s100x100_tt6&_nc_cat=102&ccb=1-7&_nc_sid=b70caf&_nc_eui2=AeEaKMnHr1vCA11g1LdslTI2UpV7fVOgZ1hSlXt9U6BnWE2V2JY3udQ9lob3o5--eAXj09fwbb0Y3_Qe6vS3bm4x&_nc_ohc=-SvWJ0OlaJ8Q7kNvgFowo5d&_nc_oc=AdimdlpIcl84OkMm2AAsQ2JYF_xWfDUBYU534D6FmLIIHelzbEMHTcRC7IrpkUzHaamdqj4Sfeb2m1t8poV69fHY&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&oh=03_Q7cD1gE6e6CpA126rlGjUOhiNanACKlXrmCXyIOWofNrCwE8WA&oe=67E23070", name: "Làng Gốm Nhị Tràng", url: "https://www.facebook.com/messages/t/8073160506112171" }
-    ];
+let num_mess = 0;
+let API_KEY;
+const old_HTML = document.querySelector(".popup-container").innerHTML;
+
+window.onload = function() {
+    chrome.storage.local.get(["API_KEY"], function(result) {
+        API_KEY = result.API_KEY;
+
+        if (API_KEY == undefined) {
+            document.querySelector(".popup-container").innerHTML = `
+                <p style="margin: 0px;">Take API key in this link: </p>
+                <a href="https://aistudio.google.com/apikey">https://aistudio.google.com/apikey</a>
+                <div style="margin-top: 10px;" class="input-container">
+                    <input style="padding: 8px;" type="text" id="searchInput" placeholder='Paste API key here (Enter)' font-size=12px>
+                </div>
+            `;
+
+            document.getElementById("searchInput").addEventListener("keydown", function(event) {
+                if (event.key === "Enter") {
+                    event.preventDefault();
+
+                    API_KEY = document.getElementById("searchInput").value;
+                    chrome.storage.local.set({ "API_KEY": API_KEY });
+                    document.querySelector(".popup-container").innerHTML = old_HTML;
+                    window.onload();
+                }
+            });
+        }
+        else {
+            const getInfo = (url) => {
+                chrome.runtime.sendMessage({ type: "getInfo", url: url }, async (response) => {
+                    const new_messages = [{
+                        avatar: response.avatar,
+                        name: response.name,
+                        url: url
+                    }];
+
+                    Request_(new_messages);
+                });
+            };
+
+            chrome.runtime.sendMessage({ type: "checkUnread" }, async (response) => {
+                response.forEach((url) => { getInfo(url); });
+                if (!response.length && !num_mess) {
+                    document.querySelector(".chat-list").innerHTML = '<div class="chat-item"> <div class="chat-info", style="font-size: medium;">No unread messages</div> </div>';
+                };
+            });
+
+            document.getElementById("searchInput").addEventListener("keydown", function(event) {
+                if (event.key === "Enter") {
+                    event.preventDefault();
+
+                    const url = document.getElementById("searchInput").value;
+                    getInfo(url);
+                    document.getElementById("searchInput").value = "";
+                }
+            });
+        }
+    });
+};
+
+
+function Request_(messages) {
     let urls = [];
     messages.forEach(chat => {urls.push(chat.url)});
 
@@ -12,12 +70,13 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error("Error receiving response:", chrome.runtime.lastError);
         } else {
             for (let i = 0; i < urls.length; i++) {
-                document.querySelectorAll(".message")[i].innerText = "await summarizeMessages(response[i])/////////////////////////////////////";
+                document.querySelectorAll(".message")[num_mess-1].innerText = await summarizeMessages(response[i]);
             };
         };
     });
 
     let chatList = document.querySelector(".chat-list");
+    if (!num_mess) {chatList.innerHTML = "";}
     messages.forEach(chat => {
         let chatItem = document.createElement("div");
         chatItem.classList.add("chat-item");
@@ -28,9 +87,23 @@ document.addEventListener("DOMContentLoaded", () => {
                 <div class="message">Loading...</div>
             </div>
         `;
+
+        chatItem.addEventListener("click", () => {
+            chrome.tabs.create({ url: chat.url, active: false });
+        });
+
+        chatItem.addEventListener("mouseenter", () => {
+            chatItem.style.backgroundColor = "#4d4d4d";
+          });
+
+        chatItem.addEventListener("mouseleave", () => {
+            chatItem.style.backgroundColor = "#313335";
+        });
+
         chatList.appendChild(chatItem);
+        num_mess++;
     });
-});
+}
 
 // Hàm gọi API Gemini để tóm tắt nội dung tin nhắn
 async function summarizeMessages(messages) {
@@ -45,9 +118,8 @@ Lưu ý: Nếu <Phần tin nhắn chính> không có, nghĩa là nó là hình �
 ${messages.join("\n")}
 `;
 
-    const API_KEY = "AIzaSyDWzCBbPjy_yB63k_bXy5PjgVJ9CBk4Ecs";
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-8b:generateContent?key=${API_KEY}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ contents: [{ role: "user", parts: [{ text: messages }] }] })
